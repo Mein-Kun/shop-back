5773fa3913ea240392962590362ebdf8a6dabc41
Denis <kbs.pstu@gmail.com>
2023-12-15 12:30:33 +0500

    commit number 6

--- src/auth/authentificated.guard.ts
+++ src/auth/authentificated.guard.ts
@@ -1,10 +1,23 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+// import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+// @Injectable()
+// export class AuthenticatedGuard implements CanActivate {
+//   async canActivate(context: ExecutionContext) {
+//     const request = context.switchToHttp().getRequest();
+
+//     return request.isAuthenticated();
+//   }
+// }
+
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class AuthenticatedGuard implements CanActivate {
-  async canActivate(context: ExecutionContext) {
+export class LocalAuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-
     return request.isAuthenticated();
   }
 }
--- src/auth/local.auth.guard.ts
+++ src/auth/local.auth.guard.ts
@@ -1,13 +1,26 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+// import { ExecutionContext, Injectable } from '@nestjs/common';
+// import { AuthGuard } from '@nestjs/passport';
+
+// @Injectable()
+// export class LocalAuthGuard extends AuthGuard('local') {
+//   async canActivate(context: ExecutionContext) {
+//     const result = (await super.canActivate(context)) as boolean;
+//     const request = context.switchToHttp().getRequest();
+
+//     await super.logIn(request);
+//     return result;
+//   }
+// }
+
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {
-  async canActivate(context: ExecutionContext) {
-    const result = (await super.canActivate(context)) as boolean;
+export class LocalAuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-
-    await super.logIn(request);
-    return result;
+    return validateRequest(request);
   }
 }
