624c7f13b4c8849f046b9851f496b0c36827dd2e
Denis <kbs.pstu@gmail.com>
2023-12-17 14:09:59 +0500

    commit number 18

--- src/auth/authentificated.guard.ts
+++ src/auth/authentificated.guard.ts
@@ -14,9 +14,7 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
     console.log(request)
     // return request.isAuthenticated();
