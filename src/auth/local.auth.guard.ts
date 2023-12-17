868460ad1bbd9029fe6a5d3e703859d88bc08ca0
Denis <kbs.pstu@gmail.com>
2023-12-17 14:00:39 +0500

    commit number 17

--- src/auth/local.auth.guard.ts
+++ src/auth/local.auth.guard.ts
@@ -2,28 +2,28 @@ import { ExecutionContext, HttpException, HttpStatus, Injectable, UnauthorizedEx
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-// export class LocalAuthGuard extends AuthGuard('local') {
-//   async canActivate(context: ExecutionContext) {
-//     const result = (await super.canActivate(context)) as boolean;
-//     const request = context.switchToHttp().getRequest();
-
-//     await super.logIn(request);
-//     return result;
-//   }
-// }
 export class LocalAuthGuard extends AuthGuard('local') {
-  handleRequest(err, user, _info, context, _status) {
+  async canActivate(context: ExecutionContext) {
+    const result = (await super.canActivate(context)) as boolean;
     const request = context.switchToHttp().getRequest();
-    const { mobile, password } = request.body;
-    if (err || !user) {
-      if (!mobile) {
-        throw new HttpException({ message: 'no mobile' }, HttpStatus.OK);
-      } else if (!password) {
-        throw new HttpException({ message: 'no pasword' }, HttpStatus.OK);
-      } else {
-        throw err || new UnauthorizedException();
-      }
-    }
-    return user;
+
+    await super.logIn(request);
+    return result;
   }
 }
+// export class LocalAuthGuard extends AuthGuard('local') {
+//   handleRequest(err, user, _info, context, _status) {
+//     const request = context.switchToHttp().getRequest();
+//     const { mobile, password } = request.body;
+//     if (err || !user) {
+//       if (!mobile) {
+//         throw new HttpException({ message: 'no mobile' }, HttpStatus.OK);
+//       } else if (!password) {
+//         throw new HttpException({ message: 'no pasword' }, HttpStatus.OK);
+//       } else {
+//         throw err || new UnauthorizedException();
+//       }
+//     }
+//     return user;
+//   }
+// }
