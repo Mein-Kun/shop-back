21265fb7d0745e5c6d06ed4d6d6e153b5ec05c18
Denis <kbs.pstu@gmail.com>
2023-12-18 14:52:07 +0500

    commit 3

--- src/auth/constant.ts
+++ src/auth/constant.ts
@@ -1,4 +1,13 @@
 import { SetMetadata } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext().req.user;
+  },
+);
 
 export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
