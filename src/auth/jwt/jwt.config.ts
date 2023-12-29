cfb34386a2d03f52c791069faf1ad298b0381007
Denis <kbs.pstu@gmail.com>
2023-12-29 15:32:24 +0500

    add jwtservice

--- src/auth/jwt/jwt.config.ts
+++ src/auth/jwt/jwt.config.ts
@@ -1,9 +1,6 @@
-import { ConfigService } from '@nestjs/config'
 import { JwtModuleOptions } from '@nestjs/jwt'
 
-export const getJwtConfig = async (
-	configService: ConfigService
-): Promise<JwtModuleOptions> => ({
-	secret: configService.get(`${process.env.JWT_SECRET}`),
+export const getJwtConfig = async (): Promise<JwtModuleOptions> => ({
+	secret: `${process.env.JWT_SECRET}`,
 	signOptions: { expiresIn: '60s' },
 })
