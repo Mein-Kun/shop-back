e7cb6e20220cd771317f201f3f5be2e4184a9c87
Denis <kbs.pstu@gmail.com>
2023-12-29 14:25:51 +0500

    add cors

--- src/main.ts
+++ src/main.ts
@@ -17,21 +17,23 @@ async function bootstrap() {
       saveUninitialized: false,
     }),
   );
+  var cors = require('cors')
+  app.use(cors())
   app.use(passport.initialize());
   app.use(passport.session());
   // app.setGlobalPrefix('api')
 
-  app.enableCors({
-    credentials: true,
-    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization, Observe',
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    origin: true,
-    // origin: [
-    //   'https://landmotors-client.onrender.com',
-    //   // 'https://landmotors-server.onrender.com',
-    //   // 'http://localhost:3001',
-    // ],
-  });
+  // app.enableCors({
+  //   credentials: true,
+  //   allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization, Observe',
+  //   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  //   origin: true,
+  //   // origin: [
+  //   //   'https://landmotors-client.onrender.com',
+  //   //   // 'https://landmotors-server.onrender.com',
+  //   //   // 'http://localhost:3001',
+  //   // ],
+  // });
   // app.enableCors()
 
   await app.listen(process.env.PORT || 3001);
