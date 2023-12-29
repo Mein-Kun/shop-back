0e88b990a185183cf51010e12df1ff4267321756
Denis <kbs.pstu@gmail.com>
2023-12-29 14:19:33 +0500

    add origin

--- src/main.ts
+++ src/main.ts
@@ -19,22 +19,18 @@ async function bootstrap() {
   );
   app.use(passport.initialize());
   app.use(passport.session());
-  // app.use(function(_req, res, next) {
-  //   res.header("Access-Control-Allow-Origin", "https://landmotors-client.onrender.com"); // Update to match the domain you will make the request from
-  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  //   next();
-  // });
   // app.setGlobalPrefix('api')
 
   app.enableCors({
     credentials: true,
-    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Access-Control-Allow-Origin, Access-Control-Allow-Headers',
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization, Observe',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    origin: [
-      // 'https://landmotors-client.onrender.com',
-      'https://landmotors-server.onrender.com',
-      // 'http://localhost:3001',
-    ],
+    origin: true,
+    // origin: [
+    //   'https://landmotors-client.onrender.com',
+    //   // 'https://landmotors-server.onrender.com',
+    //   // 'http://localhost:3001',
+    // ],
   });
   // app.enableCors()
 
