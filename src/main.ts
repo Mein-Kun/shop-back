0e5f0b35f1bae57643e6dee8fe3fed4bfee6796e
Denis <kbs.pstu@gmail.com>
2023-12-29 14:40:40 +0500

    cors 2

--- src/main.ts
+++ src/main.ts
@@ -23,19 +23,19 @@ async function bootstrap() {
   app.use(passport.session());
   // app.setGlobalPrefix('api')
 
-  // app.enableCors({
-  //   credentials: true,
-  //   allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization, Observe',
-  //   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-  //   origin: true,
-  //   // origin: [
-  //   //   'https://landmotors-client.onrender.com',
-  //   //   // 'https://landmotors-server.onrender.com',
-  //   //   // 'http://localhost:3001',
-  //   // ],
-  // });
+  app.enableCors({
+    credentials: true,
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization, Observe',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    origin: true,
+    // origin: [
+    // 'https://landmotors-client.onrender.com',
+    //   // 'https://landmotors-server.onrender.com',
+    //   // 'http://localhost:3001',
+    // ],
+  });
   // app.enableCors()
 
-  await app.listen(process.env.PORT || 3001, function () {console.log('CORS-enabled web server listening on port 80')});
+  await app.listen(process.env.PORT || 3001, function () {console.log('CORS-enabled web server listening on port 3001')});
 }
 bootstrap();
