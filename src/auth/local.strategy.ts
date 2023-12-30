2887a6442d5e7237241efe68706875dc054e62d9
Denis <kbs.pstu@gmail.com>
2023-12-30 21:04:57 +0500

    Finaly commit 2

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -30,7 +30,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   
   async validate(username: string, password: string): Promise<any> {
     const user = await this.authService.validateUser(
-      username,
+      username.toLowerCase(),
       password,
     );
 
