dd236408e10f1e498a4d2fd0eafeececcc8875cb
Denis <kbs.pstu@gmail.com>
2024-01-05 13:58:30 +0500

    add auth as boolean

--- src/auth/authentificated.guard.ts
+++ src/auth/authentificated.guard.ts
@@ -1,24 +1,23 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AuthenticatedGuard implements CanActivate {
-  async canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-    return request.isAuthenticated();
-  }
-}
-
-// import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-// import { Observable } from 'rxjs';
+// import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 
 // @Injectable()
 // export class AuthenticatedGuard implements CanActivate {
-//   canActivate(
-//     context: ExecutionContext,
-//   ): boolean | Promise<boolean> | Observable<boolean> {
+//   async canActivate(context: ExecutionContext) {
 //     const request = context.switchToHttp().getRequest();
-//     // console.log(request)
 //     return request.isAuthenticated();
-//     // return true;
 //   }
 // }
+
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthenticatedGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    return request.isAuthenticated();
+    // return true;
+  }
+}
